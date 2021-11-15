8769b8ee69002521e0560639f05fbb80f9e1d148
fedelosio99 <federico.losio@ing.austral.edu.ar>
2021-11-15 15:30:20 -0300

    Cambie el MainRobot

--- UrScript/mainRobot.py
+++ UrScript/mainRobot.py
@@ -7,17 +7,17 @@ import numpy as np
 import urx
 
 
-from Regression import regresion_x as rx,regresion_y as ry
+# from Regression import regresion_x as rx,regresion_y as ry
 from urx.robotiq_two_finger_gripper import Robotiq_Two_Finger_Gripper
-
+#
 # from mainapu import MainVA as va
-
+#
 #
 # x= rx(0.3088)
 # y= ry(0.0754)
-Rad=pi/3
+# Rad=pi/3
 # va(0)
-#
+
 # print('predicted response:', x,y, sep='\n')
 # Create robot instance
 
@@ -34,7 +34,7 @@ try:
     pose_1 = [2, -1.6088158092894496, 1.4181464354144495, -1.391148881321289, -1.5592621008502405, 1.0196056365966797]
 
     # Move robot to new position
-    pose_in = [2.5512642860412598, -1.6088158092894496, 1.4181464354144495, -1.391148881321289, -1.5592621008502405, 1.0196056365966797]
+    pose_in = [3.1093735694885254, -1.6083599529662074, 1.417511288319723, -1.3918422025493165, -1.575754467641012, 1.5372142791748047]
 
     a=0.2
     v=0.3
@@ -44,13 +44,8 @@ try:
     print("Moving to ", pose_in)
     rob.movej(pose_in, a, v ,wait=True)
 
-    # # # Cambie el is_running por el is_program_running en el wait for move
-    #
-    # time.sleep(1)
-    #
-    # move = rob.getl()
-    #
-    # time.sleep(1)
+     # Cambie el is_running por el is_program_running en el wait for move
+
     #
     # move[0] = x
     # move[1] = y
