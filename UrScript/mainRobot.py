234095dd1b3defe5df5b2ac8ce4b6d70e8bc3486
fedelosio99 <federico.losio@ing.austral.edu.ar>
2021-11-29 13:51:16 -0300

    pato es gay

--- UrScript/mainRobot.py
+++ UrScript/mainRobot.py
@@ -5,11 +5,22 @@ import urx
 
 from Regression import regresion_x as rx, regresion_y as ry
 from urx.robotiq_two_finger_gripper import Robotiq_Two_Finger_Gripper
+rob = urx.Robot("192.168.0.16")
+gripper = Robotiq_Two_Finger_Gripper(rob)
 
 
+pose_in = [3.1093735694885254, -1.6083599529662074, 1.417511288319723, -1.3918422025493165, -1.575754467641012,
+                   1.5372142791748047]
+
+a = 0.3
+v = 0.5
+
+# rob.movej(pose_in, a, v, wait=True)
+move = rob.getl()
+print(move)
 
 def robot(x, y, angle, contador):
-    contador +=1
+
     rob = urx.Robot("192.168.0.16")
     gripper = Robotiq_Two_Finger_Gripper(rob)
 
@@ -19,19 +30,17 @@ def robot(x, y, angle, contador):
         init_joint_angles = rob.getj()
         print("Initial joint position is ", init_joint_angles)
 
-        # pose_1 = [3.2759432792663574, -1.2038753789714356, 1.8174002806292933, -2.2467409572997035, -1.5875690619098108,
-        #           2.2951483726501465]
-        pose_1 = [1.7542033195495605, -1.9955908260741175, 2.6349318663226526, -2.1839128933348597, -1.5938661734210413,
-         0.8319950103759766]
+
+        pose_l = [0.4166700510736631+(contador/100), 0.17632951082862758, 0.3693618977251729, 0.0013343769722733135, 3.127955852012821, -0.0065696793773294465]
+
         # Move robot to new position
-        pose_in = [3.1093735694885254, -1.6083599529662074, 1.417511288319723, -1.3918422025493165, -1.575754467641012,
-                   1.5372142791748047]
+        pose_j = [3.2224669456481934, -1.6972586117186488, 1.4982417265521448, -1.3831556600383301, -1.5768340269671839, 1.65024995803833]
 
         a = 0.3
         v = 0.5
 
-        print("Moving to ", pose_in)
-        rob.movej(pose_in, a, v, wait=True)
+        print("Moving to ", pose_j)
+        rob.movej(pose_j, a, v, wait=True)
 
         # Cambie el is_running por el is_program_running en el wait for move
 
@@ -58,11 +67,14 @@ def robot(x, y, angle, contador):
         print("Closing")
         gripper.close_gripper()
 
-        print("Moving to ", pose_gripper)
+        print("Subiendo ")
         rob.movel(subir, a, v, wait=True, relative=True)
 
-        print("Moving to ", pose_1)
-        rob.movej(pose_1, a, v, wait=True)
+        print("Moving to ", pose_l)
+        rob.movel(pose_l, a, v, wait=True)
+
+        print("Bajando")
+        rob.movel(pose_gripper, a, v, wait=True, relative=True)
 
         print("Dejando pieza")
         gripper.open_gripper()
@@ -74,5 +86,7 @@ def robot(x, y, angle, contador):
         # Close robot connection
         print("Closing robot connection")
         rob.close()
+
+    contador += 1
 
     return contador
